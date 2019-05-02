b0c503374b3ed2fa513cb7ddecc4aec791ef403c
seomoongyo <smk354@naver.com>
2019-05-02 22:56:56 +0900

    333

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -12,7 +12,7 @@ export class ListPage {
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavControlle111, public navParams: NavParams) {
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
 // 여기다가함
